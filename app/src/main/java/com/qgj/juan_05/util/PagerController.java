f543d25f1fb24161b29d05ea0aa1b1bb15f86738
zhizhuo <2455693921@qq.com>
2021-11-08 15:47:58 +0800

    修改导航工具类

--- app/src/main/java/com/qgj/juan_05/util/PagerController.java
+++ app/src/main/java/com/qgj/juan_05/util/PagerController.java
@@ -12,7 +12,7 @@ public class PagerController {
             case "metro_query/index":
         //        controller.navigate(R.id.metro_fragment,new Bundle());
                 break;
-            case "living_expenses/index":
+            case "demo/index":
                 controller.navigate(R.id.wearther_fragment,new Bundle());
                 break;
             case "park/index":
